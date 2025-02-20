15ffbe7c8f6650b8664768f0bca116401fbc4385
Taahh <thepheonixblader@gmail.com>
2025-02-20 05:44:01 -0800

    erm

--- src/components/protected.tsx
+++ src/components/protected.tsx
@@ -1,26 +1,28 @@
 import React from "react";
 import { getServerSession } from "next-auth";
 import { headers } from "next/headers";
-import { options } from "@/utils/auth";
 import SignIn from "./auth/redirect";
+import { options } from "@/utils/auth";
 import { Role } from "shared";
-
+import { redirect } from "next/navigation";
 const ProtectedPage = async ({
   children,
   role = "User",
 }: {
   children: React.ReactNode;
   role?: Role;
-}): Promise<JSX.Element> => {
-  console.log(role);
+}) => {
   const session = await getServerSession(options);
   const header = await headers();
   const pathName = header.get("x-url") || "";
   if (!session) {
     return <SignIn callback={pathName} />;
   }
+  if (role != session.user.role) {
+    redirect("/user");
+  }
 
-  return children;
+  return <>{children}</>;
 };
 
 export default ProtectedPage;
