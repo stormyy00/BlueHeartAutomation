4aaa69c613d025bda1b948c8c83533e848054432
seanquiambao <slquiambao03@gmail.com>
2025-02-27 11:07:16 -0800

    mailchimp setup

---
+++ src/data/newsletter/publish.ts
@@ -0,0 +1,12 @@
+export const CAMPAIGN = {
+  type: "regular",
+  recipients: {
+    list_id: "your-list-id",
+  },
+  settings: {
+    subject_line: "Your Newsletter Subject",
+    title: "Your Campaign Title",
+    from_name: "Your Name",
+    reply_to: "your@email.com",
+  },
+};
