0d808b4d7511d2801fd22a109df280cbfac4745f
Taah <admin@taah.dev>
2025-03-10 04:43:22 -0700

    email endpoint

---
+++ src/app/api/email/route.ts
@@ -0,0 +1,27 @@
+import { sendEmail } from "@/utils/email";
+import { NextRequest, NextResponse } from "next/server";
+
+type Props = {
+  subject: string;
+  recipients: string[];
+  body: string;
+  scheduled?: number;
+};
+
+export const PUT = async (req: NextRequest) => {
+  const data = (await req.json().catch(() => undefined)) as Props | undefined;
+  if (!data) {
+    return NextResponse.json(
+      {
+        message:
+          "Please provide the email's subject, recipients, an optional send date, and the body.",
+      },
+      { status: 400 },
+    );
+  }
+  const result = await sendEmail(data.subject, data.body, data.recipients);
+  return NextResponse.json(
+    { message: "Email sent!", id: result.messageId },
+    { status: 200 },
+  );
+};
