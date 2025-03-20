c3d21b3cc4bfbc5ac1d9b34687b4529f77ab8664
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-03-20 09:15:04 -0700

    yesss

--- server/src/index.ts
+++ server/src/index.ts
@@ -197,7 +197,8 @@ const onKeyExpired = async (expiredKey: string) => {
   // Place your custom logic here (e.g., sending an HTTP request or updating your app state)
   const document = await getDoc(doc(collection(db, "newsletters"), expiredKey));
   const data = document.data();
-  const organizationDoc = await getOrg(data?.orgId);
+  if (!data) return;
+  const organizationDoc = await getOrg(data.orgId);
 
   const newsletterBody = data.newsletter;
   let body = "Body here";
