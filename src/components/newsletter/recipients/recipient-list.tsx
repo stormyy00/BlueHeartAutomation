f14c05c68915ffe695dc0b8e47eabcf571d6c9c0
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2025-03-19 18:29:57 -0700

    styled recipients page

--- src/components/newsletter/recipients/recipient-list.tsx
+++ src/components/newsletter/recipients/recipient-list.tsx
@@ -9,7 +9,7 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { Organization, RecipientGroup } from "@/data/types";
-import { Pen, Info, User, X } from "lucide-react";
+import { Pen, User, X } from "lucide-react";
 import { KeyboardEvent, useState } from "react";
 import { toast } from "sonner";
 import RecipientToolbar from "./recipient-toolbar";
@@ -123,14 +123,14 @@ const Recipients = ({ org }: Props) => {
                       }
                     });
                   }}
-                  className="h-5 w-5 text-gray-900 rounded-full border-gray-300 focus:ring-gray-900"
+                  className="h-5 w-5 text-gray-900 rounded-full border-gray-500 focus:ring-gray-900"
                 />
                 RECIPIENT GROUP
               </span>
               <span className="font-bold text-gray-900 text-2xl">
                 {group.name}
               </span>
-              <span className="mt-4 flex items-center rounded-xl bg-ttickles-lightblue w-14 h-8 py-4 px-1 gap-x-1 text-sm text-white justify-center">
+              <span className="mt-4 flex items-center rounded-xl font-semibold bg-ttickles-lightblue w-14 h-8 py-4 px-1 gap-x-1 text-sm text-white justify-center">
                 <User size={20} /> {group.emails.length}
               </span>
             </div>
@@ -145,10 +145,10 @@ const Recipients = ({ org }: Props) => {
           </div>
         ))}
         <Dialog open={open} onOpenChange={(newOpen) => setOpen(newOpen)}>
-          <DialogContent className="flex flex-col gap-3 bg-white p-4 rounded-lg shadow-xl">
+          <DialogContent className="flex flex-col gap-3 bg-white p-6 rounded-lg shadow-xl">
             <DialogTitle>Edit Recipients</DialogTitle>
-            <DialogDescription className="flex items-center gap-x-1 ml-2">
-              <Info /> Edit the recipients attached to this group
+            <DialogDescription className="flex items-center gap-x-1">
+              Edit the recipients attached to this group
             </DialogDescription>
             <div className="flex flex-col gap-y-4 mt-4">
               <div className="flex flex-wrap items-center gap-2 p-2 border border-gray-300 rounded focus-within:border-blue-500 overflow-auto">
@@ -156,7 +156,7 @@ const Recipients = ({ org }: Props) => {
                   {group[1].emails.map((email, index) => (
                     <li
                       key={index}
-                      className="px-2 py-1 bg-ttickles-darkblue text-white rounded-md flex items-center gap-x-1"
+                      className="px-2 py-1 bg-ttickles-darkblue text-white font-bold rounded-md flex items-center gap-x-2"
                     >
                       {email} <X size={20} />
                     </li>
@@ -175,7 +175,7 @@ const Recipients = ({ org }: Props) => {
             <div className="flex flex-row self-end gap-2">
               <DialogClose asChild>
                 <Button
-                  className="px-3 py-1 rounded bg-white text-black border-black hover:bg-white hover:text-black"
+                  className="px-3 py-1 rounded bg-white text-black  hover:bg-white hover:text-black border border-gray-100"
                   onClick={() => {
                     setOpen(false);
                     setGroup([
@@ -188,11 +188,11 @@ const Recipients = ({ org }: Props) => {
                   }}
                   disabled={fetching}
                 >
-                  Exit
+                  Cancel
                 </Button>
               </DialogClose>
               <Button
-                className="bg-ttickles-blue text-white px-3 py-1 rounded"
+                className="bg-ttickles-blue text-white px-3 py-1 rounded hover:bg-ttickles-blue hover:brightness-110"
                 onClick={handleSave}
                 disabled={fetching}
               >
