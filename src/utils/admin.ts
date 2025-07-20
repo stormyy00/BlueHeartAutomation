d7d0d45153aeacb8b397d42be187722cfd2a02a8
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-07-19 21:49:06 -0700

    change

--- src/utils/admin.ts
+++ src/utils/admin.ts
@@ -5,10 +5,7 @@ const app = initializeApp({
   credential: cert({
     projectId: process.env.NEXT_PRIVATE_FIREBASE_PROJECT_ID,
     clientEmail: process.env.NEXT_PRIVATE_FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.NEXT_PRIVATE_FIREBASE_PRIVATE_KEY?.replace(
-      /\\n/g,
-      "\n",
-    ),
+    privateKey: process.env.NEXT_PRIVATE_FIREBASE_PRIVATE_KEY
   }),
 });
 
